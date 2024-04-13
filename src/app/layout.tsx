b7c114816551979690734cac5953ed452bb6168b
VladYoSlav <vaifaer@gmail.com>
2024-04-13 10:33:07 +0300

    chore: background mask

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
             'bg-grid-black/[0.025] dark:bg-grid-white/[0.025]'
           )}
         >
-          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_30%,black)] dark:bg-black" />
+          <div className="pointer-events-none fixed inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_40%,black)] dark:bg-black" />
           <div className="relative">{children}</div>
         </main>
         <Toaster />
